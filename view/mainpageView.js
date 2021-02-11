f95260e4acd43bb4c4600587a1c9da79ac0eee7a
Kristian <kristian.risvaer@gmail.com>
2021-02-11 10:42:49 +0100

    Create mainpageView.js

---
+++ view/mainpageView.js
@@ -0,0 +1,21 @@
+function mainpageView() {
+    let html = '';
+    html += `
+    <div class="mainContent">
+    <div class="header">
+
+    </div>
+    <div class="profile">
+
+    </div>
+    <div class="content">
+        <div class="navigate">
+            
+        </div>
+    </div>
+    <div class="footer">
+
+    </div>
+</div>
+    `;
+}
