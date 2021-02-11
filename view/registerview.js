50b548511c07767f5bbc3bd6c3115933c58f8d3a
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-02-11 10:57:32 +0100

    asd

--- view/registerview.js
+++ view/registerview.js
@@ -4,21 +4,25 @@ registerView();
 function registerView() {
     let html = '';
     
+    
     html += `
             <div class="mainContent">
                 <div class="header">
-                    asd
+                    
                 </div>
                 <div class="profile">
-                asd
+                    
                 </div>
                 <div class="content">
+                    <h4>Velkommen til <i>Siste kjærlighet</i> 💕</h4>
                     <div class="navigate">
-                    asd
+                        <form action="#">
+                        
+                        </form>
                     </div>
                 </div>
                 <div class="footer">
-                asd
+                    
                 </div>
             </div>
            `;
