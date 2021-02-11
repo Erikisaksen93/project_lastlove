0ddbc4048ceb2dc7c1a3dcfbceabe71777aa66b1
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-02-11 10:34:28 +0100

    grid

---
+++ view/registerview.js
@@ -0,0 +1,27 @@
+let htmlDiv = document.getElementById('app');
+
+
+function registerView() {
+    let html = '';
+    
+    html += `
+            <div class="mainContent">
+                <div class="header">
+
+                </div>
+                <div class="profile">
+
+                </div>
+                <div class="content">
+                    <div class="navigate">
+                        
+                    </div>
+                </div>
+                <div class="footer">
+
+                </div>
+            </div>
+           `;
+
+           htmlDiv.innerHTML = html;
+}
