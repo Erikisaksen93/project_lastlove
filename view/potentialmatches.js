c21db48e5dcf6d6d63b349adc837c295890cb3c5
Kristian <kristian.risvaer@gmail.com>
2021-02-11 13:23:44 +0100

    sdf

---
+++ view/potentialmatches.js
@@ -0,0 +1,34 @@
+potentialMatchesView();
+function potentialMatchesView() {
+    let match = model.possibleMatches[model.possibleMatchesIndex];
+    let html = '';
+    html += `
+    <div class="mainContent">
+        <div class="header">
+            <button id="backToMainpageButton" onclick="mainpageView()" style="float: left;">Tilbake til hovedsiden</button>
+            <h2 id="potentialMatchesHeader">Dette er profilen til ${match.firstname}.<br> Syntes du ${match.firstname} virket interessant, la ${match.gender == 'Kvinne' ? 'henne' : 'han'} få vite det ved å trykke på hjertet nederst til venstre. <br>Du kan også trykke på Meldingknappen nederst til høyre for å sende en melding til ${match.firstname}</h2>
+        </div>
+        <div class="profile">
+        <div id="yourProfileImageDiv">
+        <img src="${match.profile_image}" style="width:100%;height:100%;">
+    </div>
+        </div>
+        <div class="content">
+            <div class="navigate">
+                <div id="" class="userProfileInfo">${match.firstname}</div>
+                <div id="" class="userProfileInfo">${match.age} år</div>
+                <div id="" class="userProfileInfo">${match.gender}</div>
+                <div id="" class="userProfileInfo">Søker en ${match.genderWanted}</div>
+                <div id="" class="userProfileInfo">${match.zipcode}</div>
+                <div id="userProfileDescription" class="userProfileInfo">Litt om meg:<br><br>${match.description}</div>
+                <button id="potentialMatchesNextButton">Neste Single</button>
+            </div>
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+    `;
+
+    document.getElementById('app').innerHTML = html;
+}
--- view/yourprofileview.js
+++ view/yourprofileview.js
@@ -1,10 +1,10 @@
-yourProfileView();
 function yourProfileView() {
     let html = '';
     html += `
     <div class="mainContent">
     <div class="header">
-
+        <button id="backToMainpageButton" onclick="mainpageView()" style="float: left;">Tilbake til hovedsiden</button>
+        <h2 id="yourProfileHeader">Dette er hvordan alle andre ser profilen din. Hvis det er noe du vil endre på så kan du trykke på det du vil endre.</h2>
     </div>
     <div class="profile">
         <div id="yourProfileImageDiv">
@@ -16,16 +16,15 @@ function yourProfileView() {
     </div>
     <div class="content">
         <div class="navigate">
-        <div>${model.userProfile.firstname}</div>
-            <div>${model.userProfile.age} år</div>
-            <div>${model.userProfile.gender}</div>
-            <div>Søker en ${model.userProfile.genderWanted}</div>
-            <div>${model.userProfile.zipcode}</div>
+            <div id="" class="userProfileInfo">${model.userProfile.firstname}</div>
+            <div id="" class="userProfileInfo">${model.userProfile.age} år</div>
+            <div id="" class="userProfileInfo">${model.userProfile.gender}</div>
+            <div id="" class="userProfileInfo">Søker en ${model.userProfile.genderWanted}</div>
+            <div id="" class="userProfileInfo">${model.userProfile.zipcode}</div>
+            <div id="userProfileDescription" class="userProfileInfo">Litt om meg:<br><br>${model.userProfile.description}</div>
         </div>
-        </div>
-        <div class="footer">
-        ${model.userProfile.description}
-
+    </div>
+    <div class="footer">
     </div>
 </div>
     `;
